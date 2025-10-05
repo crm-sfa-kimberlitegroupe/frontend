f3e08b05f6e8bbcb57e8e234b0a849a60f1eaaae
OFFO ANGE <andrepierre585@gmail.com>
2025-10-05 19:12:28 +0000

    mise a jours

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -45,12 +45,7 @@ export default function LoginPage() {
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Connexion
           </h2>
-          <p className="mt-2 text-center text-sm text-gray-600">
-            Ou{' '}
-            <Link to="/register" className="font-medium text-blue-600 hover:text-blue-500">
-              créer un nouveau compte
-            </Link>
-          </p>
+
         </div>
 
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
