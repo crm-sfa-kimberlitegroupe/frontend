84b6b5f5158d904bd2922bf1c2148ec3bb58a448
OFFO ANGE <andrepierre585@gmail.com>
2025-10-06 11:04:55 +0000

    mise a jours

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = 'http://backendsfa.onrender.com/api';
 
 // Types
 export interface User {
